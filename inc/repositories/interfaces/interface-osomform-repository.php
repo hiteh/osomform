cc64b09c9a20a47c768b6e2d02192a9d58379fb7
hiteh <hubert.warzycha@gmail.com>
2020-04-15 13:58:52 +0200

    Add static methods for storage setup and remove.

--- inc/repositories/interfaces/interface-osomform-repository.php
+++ inc/repositories/interfaces/interface-osomform-repository.php
@@ -4,4 +4,6 @@ interface OsomformRepositoryInterface
 {
     public function create( array $data );
     public function readAll();
+    public static function storage_setup();
+    public static function storage_remove();
 }
